9f30e92b228c6f5f070dda3e01a013bf326692e3
THeikki <t9tohe00@students.oamk.fi>
2021-11-10 19:04:07 +0200

    y

--- src/components/User.js
+++ src/components/User.js
@@ -10,51 +10,55 @@ const User = ({ onClose, user, onDelete }) => {
 
     return (
         <div className="container"> 
-            <table className="userInfo">
-                <thead>
-                    <tr>
-                        <th> User name </th>
-                    </tr>   
-                </thead>
-                <tbody>
-                     <tr>
-                         <td> { user.username } </td>
-                    </tr>  
-                </tbody>
-                <thead>
-                    <tr>
-                        <th> Playtimes </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td> { user.gameTimes } </td>
-                    </tr>
-                </tbody>
-                <thead>
-                    <tr>
-                    <th> High score </th>
-                    </tr> 
-                </thead>
-                <tbody>
-                    <tr>
-                        <td> { user.highScore } </td>
-                    </tr>
-                </tbody>
-                <thead>
-                    <tr>
-                        <th> Overall points </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td> { user.overallPoints } </td>
-                    </tr>
-                </tbody>
-            </table>
-            <button className="logoutButton" onClick={onClose}> Log out </button>
-            <p>Here you can delete user account</p>
-            <button className="deleteButton" onClick={onDelete}> Delete </button>
+            <div className="position">
+                <table className="userInfo">
+                    <thead>
+                        <tr>
+                            <th> User name </th>
+                        </tr>   
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td> { user.username } </td>
+                        </tr>  
+                    </tbody>
+                    <thead>
+                        <tr>
+                            <th> Playtimes </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td> { user.gameTimes } </td>
+                        </tr>
+                    </tbody>
+                    <thead>
+                        <tr>
+                        <th> High score </th>
+                        </tr> 
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td> { user.highScore } </td>
+                        </tr>
+                    </tbody>
+                    <thead>
+                        <tr>
+                            <th> Overall points </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td> { user.overallPoints } </td>
+                        </tr>
+                    </tbody>
+                </table>
+                <button className="logoutButton" onClick={onClose}> Log out </button>
+                <p>Here you can delete user account</p>
+                <button className="deleteButton" onClick={onDelete}> Delete </button>
+                    
+            </div>
+            
        </div>
     )
 }
