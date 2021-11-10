550743a6a5f59e25d7c2cb1df8628837932b1d5c
THeikki <t9tohe00@students.oamk.fi>
2021-11-10 18:32:44 +0200

    update

--- src/components/User.js
+++ src/components/User.js
@@ -10,7 +10,6 @@ const User = ({ onClose, user, onDelete }) => {
 
     return (
         <div className="container"> 
-            <h1>Testi</h1>
             <h2>Player statistics</h2>
             <table className="userInfo">
                 <thead>
